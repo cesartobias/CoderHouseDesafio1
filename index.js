268096747e6d0f38d37028066602b5e88baaa63e
Cesar Tobias <cesar.tobias@gmail.com>
2021-11-15 19:55:18 -0600

    se crea interface ClienteBancoInterface

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var Banco = /** @class */ (function () {
     function Banco() {
         var _this = this;
-        this.id = 1;
-        this.nombre = "Banco Banquero";
-        this.direccion = "Direccion Conocida";
-        this.telefono = "(888) 888 888";
-        // id=this.id;
-        // nombre=this.nombre;
-        // direccion=this.direccion;
-        // telefono=this.telefono;
+        this.idBanco = 1;
+        this.nombreBanco = "Banco del Norte";
+        this.dirBanco = "Av Constitucion # 1230 Col. El Paseo";
+        this.telBanco = "(888) 888 888";
         /*public*/ this.getInfoBanco = function () {
             var data = {
-                id: _this.id,
-                nombre: _this.nombre,
-                direccion: _this.direccion,
-                telefono: _this.telefono
+                id: _this.idBanco,
+                nombre: _this.nombreBanco,
+                direccion: _this.dirBanco,
+                telefono: _this.telBanco
             };
             return data;
         };
@@ -24,49 +20,88 @@ var Banco = /** @class */ (function () {
     }
     return Banco;
 }());
-console.log("***** Informacion Guardada del Banco *****");
-var banco = new Banco(); //2,"Banco","Banco","banco");
-var datosBanco = banco.getInfoBanco();
-console.log(datosBanco.direccion);
-console.log("***** Actualizacion Informacion del Banco *****");
-var updateBanco = new Banco();
-updateBanco.setInfoBanco(2, "Banco 2", "Direccion 2", "(999) 9999 999");
 var Cliente = /** @class */ (function () {
     function Cliente() {
-        this.id = 1;
-        this.nombre = "cesar Tobias";
-        this.direccion = "Direccion Desonocida";
-        this.telefono = "(9999) 999 999";
+        var _this = this;
+        this.idCliente = 1;
+        this.nombreCliente = "Cesar Tobias";
+        this.dirCliente = "Av Paseo del Valle # 120 Col. Popocatepetl";
+        this.telCliente = "(999) 9999 9999";
+        /*public*/ this.getInfocustomer = function () {
+            var data = {
+                id: _this.idCliente,
+                nombre: _this.nombreCliente,
+                direccion: _this.dirCliente,
+                telefono: _this.telCliente
+            };
+            return data;
+        };
     }
     return Cliente;
 }());
-var ClienteBanco = /** @class */ (function () {
-    function ClienteBanco(nombreCliente, pin, saldo) {
-        var _this = this;
-        this.nombreCliente = nombreCliente;
-        this.pin = pin;
-        this.saldo = saldo;
-        this.idBanco = 1;
-        this.idCliente = 1;
-        this.getSaldo = function () {
-            var data = { saldo: _this.saldo };
-        };
-        this.Abono = function () {
-            var data = { saldo: _this.saldo };
-        };
-        this.Retiro = function () {
-            var data = { saldo: _this.saldo };
-        };
-        this.GetPin = function () {
-            console.log(_this.pin);
-        };
-        this.Changepin = function (pin) {
-            return 2;
-        };
-    }
-    return ClienteBanco;
-}());
-var cliente = new ClienteBanco("Cesar", 1234, 1000);
-var saldo = cliente.getSaldo();
-//console.log(saldo);
-console.log("Nombre del Cliente: " + cliente.nombreCliente + ". PIN: " + cliente.pin + ". Saldo: " + cliente.saldo);
+var saldoCta = {
+    idBanco: 1,
+    nombreBanco: "Banco del Norte",
+    dirBanco: "Av Constitucion # 1230 Col. El Paseo",
+    telBanco: "(888) 888 888",
+    idCliente: 1,
+    nombreCliente: "Cesar Tobias",
+    dirCliente: "Av Paseo del Valle # 120 Col. Popocatepetl",
+    telCliente: "(999) 9999 9999",
+    saldo: 200,
+    fechaTransaccion: "12/12/2021"
+};
+console.log(saldoCta.nombreCliente);
+// class ClienteBanco implements ClienteBancoInterface{
+//   saldo=20;
+//   getSaldo=()=>{
+//   }
+// }
+//console.log("***** Informacion Guardada del Banco *****");
+var banco = new Banco();
+var datosBanco = banco.getInfoBanco();
+console.log("Nombre del Banco: " + datosBanco.nombre);
+console.log("Direccion: " + datosBanco.direccion);
+console.log("Telefono: " + datosBanco.telefono);
+console.log("Bienvenido " + saldoCta.nombreCliente);
+console.log("Su saldo al dia de hoy " + saldoCta.saldo);
+console.log("Fecha ultima transaccion " + saldoCta.fechaTransaccion);
+console.log("*******************************************");
+console.log("***** Actualizacion Informacion del Banco *****");
+var updateBanco = new Banco();
+updateBanco.setInfoBanco(2, "Banco 2", "Direccion 2", "(999) 9999 999");
+// class Cliente implements Cliente{
+//   id=1;
+//   nombre="cesar Tobias";
+//   direccion="Direccion Desonocida";
+//   telefono="(9999) 999 999"
+// }
+// class ClienteBanco implements ClienteBanco{
+// idBanco=1;
+// idCliente=1;
+//     constructor(
+//         public nombreCliente:string,
+//         public pin:number,
+//         public saldo:number)
+//         {
+//         }
+//         public getSaldo =()=>{
+//           let data ={saldo:this.saldo}
+//         }
+//         public Abono =()=>{
+//           let data ={saldo:this.saldo}
+//         }
+//         public Retiro =()=>{
+//           let data ={saldo:this.saldo}
+//         }
+//         public GetPin=()=>{
+//             console.log(this.pin)
+//         }
+//         public Changepin=(pin:number):number=>{
+//           return 2; 
+//         }
+// }
+// let cliente = new ClienteBanco("Cesar",1234,1000);
+// const saldo = cliente.getSaldo();
+// //console.log(saldo);
+// console.log("Nombre del Cliente: " + cliente.nombreCliente + ". PIN: " + cliente.pin + ". Saldo: " + cliente.saldo);
--- index.ts
+++ index.ts
@@ -1,48 +1,37 @@
 interface BancoInterFace{
- id:number
-  nombre:string,
-   direccion:string
-   telefono:string
+ idBanco:number,
+  nombreBanco:string,
+   dirBanco:string,
+   telBanco:string
 }
 
-interface Cliente{
-  id:number,
-  nombre: string,
-  direccion: string
-  telefono: string
-
-  getInfoBanco();
-  setInfoBanco(id:number,nombre:string, direccion:string,telefono:string):void;
+interface ClienteInterface{
+  idCliente:number,
+  nombreCliente: string,
+  dirCliente: string,
+  telCliente: string
 }
 
 
-interface ClienteBanco{
-  idBanco:number,
-  idCliente:number
-
-  //getInfoBanco();
-  //setInfoBanco(id:number,nombre:string, direccion:string,telefono:string):void;
-}
+interface ClienteBancoInterface extends ClienteInterface, BancoInterFace{
+  saldo:number,
+  fechaTransaccion:string
+  }
 
 
 
 class Banco implements BancoInterFace{
-   id=1;
-   nombre="Banco Banquero";
-   direccion="Direccion Conocida";
-   telefono="(888) 888 888"
-
-// id=this.id;
-// nombre=this.nombre;
-// direccion=this.direccion;
-// telefono=this.telefono;
+   idBanco=1;
+   nombreBanco="Banco del Norte";
+   dirBanco="Av Constitucion # 1230 Col. El Paseo";
+   telBanco="(888) 888 888"
 
   /*public*/ getInfoBanco =()=>{
     let data ={ 
-      id:this.id,
-     nombre:this.nombre,
-     direccion:this.direccion,
-     telefono:this.telefono
+      id:this.idBanco,
+     nombre:this.nombreBanco,
+     direccion:this.dirBanco,
+     telefono:this.telBanco
     }
     return data;
   }
@@ -53,63 +42,117 @@ class Banco implements BancoInterFace{
  }
 
 
-console.log("***** Informacion Guardada del Banco *****");
-let banco = new Banco();//2,"Banco","Banco","banco");
+ class Cliente implements ClienteInterface{
+  idCliente=1;
+  nombreCliente="Cesar Tobias";
+  dirCliente="Av Paseo del Valle # 120 Col. Popocatepetl";
+  telCliente="(999) 9999 9999"
+
+ /*public*/ getInfocustomer =()=>{
+   let data ={ 
+     id:this.idCliente,
+    nombre:this.nombreCliente,
+    direccion:this.dirCliente,
+    telefono:this.telCliente
+   }
+   return data;
+ }
+}
+
+var saldoCta: ClienteBancoInterface = {
+  idBanco:1,
+  nombreBanco:"Banco del Norte",
+  dirBanco:"Av Constitucion # 1230 Col. El Paseo",
+  telBanco:"(888) 888 888",
+  idCliente:1,
+  nombreCliente:"Cesar Tobias",
+  dirCliente:"Av Paseo del Valle # 120 Col. Popocatepetl",
+  telCliente:"(999) 9999 9999",
+  saldo:200,
+  fechaTransaccion:"12/12/2021"
+}
+
+console.log(saldoCta.nombreCliente)
+
+
+// class ClienteBanco implements ClienteBancoInterface{
+
+//   saldo=20;
+//   getSaldo=()=>{
+
+//   }
+// }
+
+
+
+//console.log("***** Informacion Guardada del Banco *****");
+
+
+let banco = new Banco();
 const datosBanco = banco.getInfoBanco();
-console.log(datosBanco.direccion);
+console.log("Nombre del Banco: " + datosBanco.nombre);
+console.log("Direccion: " + datosBanco.direccion);
+console.log("Telefono: " + datosBanco.telefono);
+console.log("Bienvenido " + saldoCta.nombreCliente);
+console.log("Su saldo al dia de hoy " + saldoCta.saldo);
+console.log("Fecha ultima transaccion " + saldoCta.fechaTransaccion);
+
+console.log("*******************************************");
 console.log("***** Actualizacion Informacion del Banco *****")
+
+
 let updateBanco: Banco = new Banco();
 updateBanco.setInfoBanco(2,"Banco 2", "Direccion 2","(999) 9999 999");
 
 
 
-class Cliente implements Cliente{
-  id=1;
-  nombre="cesar Tobias";
-  direccion="Direccion Desonocida";
-  telefono="(9999) 999 999"
-}
+// class Cliente implements Cliente{
+//   id=1;
+//   nombre="cesar Tobias";
+//   direccion="Direccion Desonocida";
+//   telefono="(9999) 999 999"
+// }
 
 
-class ClienteBanco implements ClienteBanco{
-idBanco=1;
-idCliente=1;
+// class ClienteBanco implements ClienteBanco{
+// idBanco=1;
+// idCliente=1;
 
-    constructor(
-        public nombreCliente:string,
-        public pin:number,
-        public saldo:number)
-        {
+//     constructor(
+//         public nombreCliente:string,
+//         public pin:number,
+//         public saldo:number)
+//         {
 
-        }
+//         }
 
-        public getSaldo =()=>{
-          let data ={saldo:this.saldo}
-        }
-
-        
-        public Abono =()=>{
-          let data ={saldo:this.saldo}
-        }
+//         public getSaldo =()=>{
+//           let data ={saldo:this.saldo}
+//         }
 
         
-        public Retiro =()=>{
-          let data ={saldo:this.saldo}
-        }
-        public GetPin=()=>{
-            console.log(this.pin)
-        }
+//         public Abono =()=>{
+//           let data ={saldo:this.saldo}
+//         }
 
-        public Changepin=(pin:number):number=>{
-          return 2; 
-        }
-}
+        
+//         public Retiro =()=>{
+//           let data ={saldo:this.saldo}
+//         }
+//         public GetPin=()=>{
+//             console.log(this.pin)
+//         }
+
+//         public Changepin=(pin:number):number=>{
+//           return 2; 
+//         }
+// }
 
 
-let cliente = new ClienteBanco("Cesar",1234,1000);
+// let cliente = new ClienteBanco("Cesar",1234,1000);
 
-const saldo = cliente.getSaldo();
+// const saldo = cliente.getSaldo();
 
-//console.log(saldo);
-console.log("Nombre del Cliente: " + cliente.nombreCliente + ". PIN: " + cliente.pin + ". Saldo: " + cliente.saldo);
+// //console.log(saldo);
+// console.log("Nombre del Cliente: " + cliente.nombreCliente + ". PIN: " + cliente.pin + ". Saldo: " + cliente.saldo);
 
